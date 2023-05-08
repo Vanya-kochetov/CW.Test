2e449e75ac3f9b739550591197720b821e673857
user <user@DESKTOP-S25JFKU>
2023-05-08 18:46:42 +0300

    Добавьте файлы проекта.

---
+++ View/Pages/PageTask/Task2Page.xaml.cs
@@ -0,0 +1,70 @@
+﻿using CW.Test.Core;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CW.Test.View.Pages.PageTask
+{
+    /// <summary>
+    /// Логика взаимодействия для TaskPage.xaml
+    /// </summary>
+    public partial class Task2Page : Page
+    {
+        public Task2Page()
+        {
+            InitializeComponent();
+        }
+        private void BtnTask1_Click(object sender, RoutedEventArgs e)
+        {
+            CoreTest.TestFrame?.Navigate(new Task1Page());
+        }
+        private void BtnTask2_Click(object sender, RoutedEventArgs e)
+        {
+            CoreTest.TestFrame?.Navigate(new Task2Page());
+        }
+        private void BtnTask3_Click(object sender, RoutedEventArgs e)
+        {
+            CoreTest.TestFrame?.Navigate(new Task3Page());
+        }
+        private void BtnTask4_Click(object sender, RoutedEventArgs e)
+        {
+            CoreTest.TestFrame?.Navigate(new Task4Page());
+        }
+        private void BtnTask5_Click(object sender, RoutedEventArgs e)
+        {
+            CoreTest.TestFrame?.Navigate(new Task5Page());
+        }
+        private void BtnTask6_Click(object sender, RoutedEventArgs e)
+        {
+            CoreTest.TestFrame?.Navigate(new Task6Page());
+        }
+        private void BtnTask7_Click(object sender, RoutedEventArgs e)
+        {
+            CoreTest.TestFrame?.Navigate(new Task7Page());
+        }
+        private void BtnTask8_Click(object sender, RoutedEventArgs e)
+        {
+            CoreTest.TestFrame?.Navigate(new Task8Page());
+        }
+        private void BtnTask9_Click(object sender, RoutedEventArgs e)
+        {
+            CoreTest.TestFrame?.Navigate(new Task9Page());
+        }
+        private void BtnTask10_Click(object sender, RoutedEventArgs e)
+        {
+            CoreTest.TestFrame?.Navigate(new Task10Page());
+        }
+        private void BtnLogout_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Вы действительно хотите выйти?",
+                                 "Системное сообщение",
+                                  MessageBoxButton.YesNo,
+                                  MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                CoreTest.TestFrame?.Navigate(new TaskPage());
+            }
+        }
+    }
+}
+
+       
+    
